918712a53b5b9273ec05e80f378cf6f88a7a56d5
devgitdlee <devgitdlee@gmail.com>
2022-01-26 19:16:38 +0900

    css 추가

--- src/components/board/Td.tsx
+++ src/components/board/Td.tsx
@@ -20,6 +20,7 @@ const Td = ({ item, handleRemove, handleEdit }: TdProps) => {
       <tr className='"bg-white border-2 border-gray-200'>
         <td className="px-4 py-3">{item.id}</td>
         <td className="px-4 py-3">{item.name}</td>
+        <td className="px-4 py-3">{item.username}</td>
         <td className="px-4 py-3">{item.email}</td>
         <td className="px-4 py-3">{item.phone}</td>
         <td className="px-4 py-3">{item.website}</td>
