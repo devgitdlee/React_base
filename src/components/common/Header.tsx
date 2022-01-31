360805ae0e883cfca76b3ffd724f289e5a30ae00
castle <gitdlee@gmail.com>
2022-01-31 19:58:56 +0900

    공통단 추가

---
+++ src/components/common/Header.tsx
@@ -0,0 +1,12 @@
+export function HeaderContainer() {
+  return;
+
+  <>
+    <div>{/* logo */}</div>
+    <div>{/* logo 후에 */}</div>
+    <div>
+      {/* menu list */}
+      <ul></ul>
+    </div>
+  </>;
+}
